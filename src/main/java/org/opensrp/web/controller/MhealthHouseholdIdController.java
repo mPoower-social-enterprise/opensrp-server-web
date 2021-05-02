5cd0e8771d4d53afc62b6af0e2f6092e3b7cb4a4
proshanto <proshanto123@gmail.com>
2021-05-02 15:43:55 +0600

    add test code for household id

--- src/main/java/org/opensrp/web/controller/MhealthHouseholdIdController.java
+++ src/main/java/org/opensrp/web/controller/MhealthHouseholdIdController.java
@@ -2,7 +2,6 @@ package org.opensrp.web.controller;
 
 import static org.springframework.http.HttpStatus.OK;
 
-import org.json.JSONArray;
 import org.opensrp.service.HouseholdIdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/household/")
 @Controller
 public class MhealthHouseholdIdController {
 	
@@ -22,54 +22,21 @@ public class MhealthHouseholdIdController {
 		this.householdIdService = householdIdService;
 	}
 	
-	@RequestMapping(value = "/household/generated-code", method = RequestMethod.GET)
+	@RequestMapping(value = "/generated-code", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<String> getHouseholdUniqueId(@RequestParam("username") String username,
 	                                                   @RequestParam("villageId") String villageId)
 	    throws Exception {
-		int[] villageIds = new int[1000];
-		String[] ids = villageId.split(",");
-		for (int i = 0; i < ids.length; i++) {
-			villageIds[i] = Integer.parseInt(ids[i]);
-		}
 		
-		/*if (villageIds[0] == 0) {
-			CustomQuery user = householdIdService.getUserId(username);
-			List<CustomQuery> locationIds = clientService.getVillageByProviderId(user.getId(), childRoleId, locationTagId);
-			int i = 0;
-			for (CustomQuery locationId : locationIds) {
-				villageIds[i++] = locationId.getId();
-			}
-		}*/
-		JSONArray array = new JSONArray();
-		//array = householdIdService.generateHouseholdId(villageIds);
-		
-		return new ResponseEntity<>(array.toString(), OK);
+		return new ResponseEntity<>(householdIdService.generateHouseholdId(username, villageId).toString(), OK);
 	}
 	
-	@RequestMapping(value = "/household/guest/generated-code", method = RequestMethod.GET)
+	@RequestMapping(value = "/guest/generated-code", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<String> getguestHouseholdUniqueId(@RequestParam("username") String username,
+	public ResponseEntity<String> getGuestHouseholdUniqueId(@RequestParam("username") String username,
 	                                                        @RequestParam("villageId") String villageId)
 	    throws Exception {
-		int[] villageIds = new int[1000];
-		String[] ids = villageId.split(",");
-		for (int i = 0; i < ids.length; i++) {
-			villageIds[i] = Integer.parseInt(ids[i]);
-		}
-		/*
-		if (villageIds[0] == 0) {
-			CustomQuery user = clientService.getUserId(username);
-			List<CustomQuery> locationIds = clientService.getVillageByProviderId(user.getId(), childRoleId, locationTagId);
-			int i = 0;
-			for (CustomQuery locationId : locationIds) {
-				villageIds[i++] = locationId.getId();
-			}
-		}*/
-		JSONArray array = new JSONArray();
-		//array = eventService.generateGuestHouseholdId(villageIds);
-		
-		return new ResponseEntity<>(array.toString(), OK);
+		return new ResponseEntity<>(householdIdService.generateGuestHouseholdId(username, villageId).toString(), OK);
 	}
 	
 }
